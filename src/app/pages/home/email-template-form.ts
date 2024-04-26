552576ef83c182ab8fd581189b99679efa38f105
pedrohenrique57 <pedro.henrique.contato369@gmail.com>
2024-04-26 19:47:58 -0300

    Implementação da lógica para Envio de emails. Adição da dependencia Emailjs Pequenas alterações de scss nos inputs alteração do html e ts para comportar angular reactive forms
    Signed-off-by: pedrohenrique57 <pedro.henrique.contato369@gmail.com>

---
+++ src/app/pages/home/email-template-form.ts
@@ -0,0 +1,5 @@
+export class EmailTemplateForm {
+  public name: string = '';
+  public email: string = '';
+  public message: string = '';
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgClass, NgIf} from '@angular/common';
+import emailjs, {type EmailJSResponseStatus} from '@emailjs/browser';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators, FormControl} from '@angular/forms';
+import {EmailTemplateForm} from './email-template-form';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgClass, ReactiveFormsModule, NgIf],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  isNameInputFocusStyle: boolean = false;
+  isEmailInputFocusStyle: boolean = false;
+  isTextareaInputFocusStyle: boolean = false;
+  emailFormToRecord!: Record<string, string>;
+  emailForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.emailForm = this.formBuilder.group({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      message: new FormControl('', [Validators.required, Validators.minLength(15)]),
+    });
+  }
+
+  sendEmail(event: Event) {
+    if (this.emailForm.valid) {
+      this.emailFormToRecord = {
+        name: this.emailForm.get('name')?.value,
+        email: this.emailForm.get('email')?.value,
+        message: this.emailForm.get('message')?.value
+      };
+
+      event.preventDefault();
+      emailjs
+        .send('Port', 'Port Template', this.emailFormToRecord , {
+          publicKey: 'UFXabJfHrWlHmvomG',
+        })
+        .then(
+          () => {
+            console.log('SUCCESS!');
+          },
+          (error) => {
+            console.log('FAILED...', (error as EmailJSResponseStatus).text);
+          },
+        );
+    } else {
+      event.preventDefault();
+      // @ts-ignore
+      document.forms['emailFormId'].reportValidity();
+    }
+  }
 }
